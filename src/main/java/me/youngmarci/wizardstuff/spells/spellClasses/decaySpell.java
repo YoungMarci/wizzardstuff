8e56fc15ca72617815b58e8acab73af0577d7f96
Kacper Duda <63373784+YoungMarci@users.noreply.github.com>
2022-01-02 22:16:40 +0100

    improved visuals

--- src/main/java/me/youngmarci/wizardstuff/spells/spellClasses/decaySpell.java
+++ src/main/java/me/youngmarci/wizardstuff/spells/spellClasses/decaySpell.java
@@ -1,15 +1,18 @@
 package me.youngmarci.wizardstuff.spells.spellClasses;
 
 import me.youngmarci.wizardstuff.Wizardstuff;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class decaySpell implements Listener {
@@ -30,11 +33,28 @@ public class decaySpell implements Listener {
 
             if (decaySpellUUID.containsKey(snowballUUID)) {
                 Player shooterPlayer = Bukkit.getPlayer(decaySpellUUID.get(snowballUUID));
+                String playerUUID = shooterPlayer.getUniqueId().toString();
                 Location placeToTeleport = event.getHitBlock().getLocation().add(0, 1, 0);
                 Location oldLocation = shooterPlayer.getLocation();
 
                 shooterPlayer.teleport(placeToTeleport);
                 shooterPlayer.sendMessage("You used decay spell! You have 5 seconds from now to decay your enemy!");
+
+
+                new BukkitRunnable() {
+                    final ArrayList<Location> locations = getCircle(oldLocation.add(0,1,0), 0.5, 10);
+                    Integer x = 1;
+                    public void run() {
+                        if (x == 10) {
+                            this.cancel();
+                        }
+                        x++;
+                        for(int i = 0; locations.size() > i; i++) {
+                            Location loc = locations.get(i);
+                            oldLocation.getWorld().spawnParticle(Particle.FLAME, loc, 0, 0 ,0 ,0);
+                        }
+                    }
+                }.runTaskTimer(wizzStuff, 0, 20);
 
                 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(wizzStuff, new Runnable() { //get player back to old position
                     public void run() {
@@ -44,5 +64,19 @@ public class decaySpell implements Listener {
                 }, 200);
             }
         }
+    }
+
+    public ArrayList<Location> getCircle(Location center, double radius, int amount) {
+        World world = center.getWorld();
+        double increment = (2 * Math.PI) / amount;
+        ArrayList<Location> locations = new ArrayList<Location>();
+        for(int i = 0;i < amount; i++)
+        {
+            double angle = i * increment;
+            double x = center.getX() + (radius * Math.cos(angle));
+            double z = center.getZ() + (radius * Math.sin(angle));
+            locations.add(new Location(world, x, center.getY(), z));
+        }
+        return locations;
     }
 }
--- src/main/java/me/youngmarci/wizardstuff/spells/spellClasses/switchPlaceSpell.java
+++ src/main/java/me/youngmarci/wizardstuff/spells/spellClasses/switchPlaceSpell.java
@@ -3,18 +3,26 @@ package me.youngmarci.wizardstuff.spells.spellClasses;
 import me.youngmarci.wizardstuff.Wizardstuff;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class switchPlaceSpell implements Listener {
 
     Wizardstuff wizzStuff;
     public Map<String, String> switchPlaceSpellUUID;
+
+    public Map<String, String> switched = new HashMap<String, String>();
 
     public switchPlaceSpell(Wizardstuff w) { // (Require instance of Wizardstuff) Accessing variables from main class (Wizardstuff)
         this.wizzStuff = w;
@@ -36,8 +44,52 @@ public class switchPlaceSpell implements Listener {
                 shooterPlayer.teleport(damagedPlayerLocation);
                 damagedPlayer.teleport(shooterLocation);
 
+                new BukkitRunnable() {
+                    final ArrayList<Location> locations = getCircle(shooterLocation.add(0,1,0), 0.5, 10);
+                    Integer x = 1;
+                    public void run() {
+                        if (x == 5) {
+                            this.cancel();
+                        }
+                        x++;
+                        for(int i = 0; locations.size() > i; i++) {
+                            Location loc = locations.get(i);
+                            shooterLocation.getWorld().spawnParticle(Particle.FLAME, loc, 0, 0 ,0 ,0);
+                        }
+                    }
+                }.runTaskTimer(wizzStuff, 0, 20);
+
+                new BukkitRunnable() {
+                    final ArrayList<Location> locations = getCircle(damagedPlayerLocation.add(0,1,0), 0.5, 10);
+                    Integer x = 1;
+                    public void run() {
+                        if (x == 5) {
+                            this.cancel();
+                        }
+                        x++;
+                        for(int i = 0; locations.size() > i; i++) {
+                            Location loc = locations.get(i);
+                            damagedPlayerLocation.getWorld().spawnParticle(Particle.FLAME, loc, 0, 0 ,0 ,0);
+                        }
+                    }
+                }.runTaskTimer(wizzStuff, 0, 20);
+
                 shooterPlayer.sendMessage("You hit " + damagedPlayer.getName() + " with a place switch spell! Places switched!");
             }
         }
+    }
+
+    public ArrayList<Location> getCircle(Location center, double radius, int amount) {
+        World world = center.getWorld();
+        double increment = (2 * Math.PI) / amount;
+        ArrayList<Location> locations = new ArrayList<Location>();
+        for(int i = 0;i < amount; i++)
+        {
+            double angle = i * increment;
+            double x = center.getX() + (radius * Math.cos(angle));
+            double z = center.getZ() + (radius * Math.sin(angle));
+            locations.add(new Location(world, x, center.getY(), z));
+        }
+        return locations;
     }
 }
